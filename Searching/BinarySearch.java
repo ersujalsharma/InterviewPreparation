a770a95e29ae91800e4be9bc7f3f624ffc9dcaae
Sujal Sharma <sujalsharmaprofessional@gmail.com>
2025-06-08 16:36:55 +0530

    Binary Search on Rotated Array.

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -23,4 +23,48 @@ public class BinarySearch {
         }
         return -1;
     }
+    // Find Minimum in Rotated Sorted Array
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        int min = Integer.MAX_VALUE;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[start]<=nums[mid]){
+                min = Math.min(nums[start],min);
+                start = mid+1;
+            }
+            else{
+                min = Math.min(nums[mid],min);
+                end = mid-1;
+            }
+        }
+        return min;
+    }
+    // Search in Rotated Sorted Array
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[start]<=nums[mid]){
+                if(nums[start]<=target && target<=nums[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[end]){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
 }
