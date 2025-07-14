498f9d84b8f8bfd651a38f355787779b3e751146
Sujal Sharma <62907239+ersujalsharma@users.noreply.github.com>
2025-07-14 17:03:42 +0530

    Heap Implementation

--- BinaryTrees/BSTImplementation.java
+++ BinaryTrees/BSTImplementation.java
@@ -1,5 +1,9 @@
 package BinaryTrees;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class BSTImplementation {
     private static class Node {
         int data;
@@ -100,6 +104,113 @@ public class BSTImplementation {
         displayPretty(node.left, prefix + (isLeft ? "│   " : "    "), true);
         displayPretty(node.right, prefix + (isLeft ? "│   " : "    "), false);
     }
+    private void preOrderIterative(Node node){
+        Stack<Node> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+            System.out.print(current.data + " ");
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+    }
+    private void inOrderIterative(Node node){
+        Stack<Node> stack = new Stack<>();
+        while (true) {
+            if(node!=null){
+                stack.push(node);
+                node = node.left;
+            }
+            else{
+                if(stack.isEmpty()) break;
+                node = stack.pop();
+                System.out.print(node.data+" ");
+                node = node.right;
+            }
+        }
+    }
+    private void PostOrderIterative(Node node){
+        Stack<Node> stack = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            node = stack.pop();
+            stack2.push(node);
+            if(node.left!=null) stack.push(node.left);
+            if(node.right!=null) stack.push(node.right);
+        }
+        while(!stack2.isEmpty()){
+            System.out.print(stack2.pop().data+" ");
+        }
+    }
+    private void postOrderIterativeStack1(Node node){
+        Stack<Node> stack = new Stack<>();
+        while(node!=null || !stack.isEmpty()){
+            if(node != null){
+                stack.push(node);
+                node = node.left;
+            }
+            else{
+                Node temp = stack.peek().right;
+                if(temp == null){
+                    temp = stack.pop();
+                    System.out.print(temp.data+" ");
+                    while(!stack.isEmpty() && temp  == stack.peek().right){
+                        temp = stack.pop();
+                        System.out.print(temp.data+" ");
+                    }
+                }
+                else{
+                    node = temp;
+                }
+            }
+        }
+    }
+    private static class Pair{
+        Node node;
+        int second;
+        public Pair(Node node,int second){
+            this.node = node;
+            this.second = second;
+        }
+    }
+    private void preInPostIterativeOrder(Node node){
+        Stack<Pair> stack = new Stack<>();
+        List<Integer> preorder = new ArrayList<>();
+        List<Integer> inorder = new ArrayList<>();
+        List<Integer> postorder = new ArrayList<>();
+        stack.push(new Pair(node, 1));
+        while(!stack.isEmpty()){
+            Pair p = stack.pop();
+            if(p.second == 1){
+                preorder.add(p.node.data);
+                p.second++;
+                stack.push(p);
+                if(p.node.left!=null){
+                    stack.push(new Pair(p.node.left, 1));
+                }
+            }
+            else if(p.second == 2){
+                inorder.add(p.node.data);
+                p.second++;
+                stack.push(p);
+                if(p.node.right!=null){
+                    stack.push(new Pair(p.node.right, 1));
+                }
+            }
+            else{
+                postorder.add(p.node.data);
+            }
+        }
+        System.out.println();
+        System.out.println(preorder);
+        System.out.println(inorder);
+        System.out.println(postorder);
+    }
     public static void main(String[] args) {
         // BSTImplementation bst = new BSTImplementation();
         // bst.insert(10);
@@ -128,5 +239,7 @@ public class BSTImplementation {
         sortedBst.display();
         System.out.println();
         sortedBst.displayPretty(sortedBst.root, "", true);
+        sortedBst.postOrderIterativeStack1(sortedBst.root);
+        sortedBst.preInPostIterativeOrder(sortedBst.root);
     }
 }
---
+++ DataStructureImplementation/heaps/MinHeap.java
@@ -0,0 +1,104 @@
+package heaps;
+
+import java.util.Arrays;
+
+public class MinHeap {
+    private int arr[];
+    private int size;
+    public MinHeap(){
+        arr = new int[16];
+        Arrays.fill(arr,-1);
+        size = 0;
+    }
+    public MinHeap(int size){
+        arr = new int[size];
+    }
+    public void add(int data){
+        if(size == arr.length) {
+            System.out.println("Overflow");
+        }
+        arr[size] = data;
+        heapifyUp(size);
+        size++;
+    }
+    private void heapifyUp(int index) {
+        // TODO Auto-generated method stub
+        while(index>0){
+            int parent = (index-1)/2;
+            if(arr[parent]<arr[index]) break;
+            swap(index,parent);
+            index = parent;
+        }
+    }
+    private void swap(int index, int parent) {
+        // TODO Auto-generated method stub
+        int temp = arr[index];
+        arr[index] = arr[parent];
+        arr[parent] = temp;
+    }
+    public void pop(){
+        if(isEmpty()) {
+            System.out.println("Underflow");
+            return;
+        }
+        // swap with the last one.
+        arr[0] = arr[size-1];
+        arr[size-1] = -1;
+        size--;
+        heapifyDown(0);
+    }
+    private void heapifyDown(int index) {
+        // TODO Auto-generated method stub
+        while(index<size){
+            int smallest = index;
+            int leftchild = (2*index)+1;
+            int rightchild = (2*index)+2;
+            if(leftchild<size && arr[leftchild]<arr[smallest]) smallest = leftchild;
+            if(rightchild<size && arr[rightchild]<arr[smallest]) smallest = rightchild;
+            if(arr[smallest]==arr[index]) break;
+            swap(index,smallest);
+            index = smallest;
+        }
+    }
+    public void peek(){
+        if(isEmpty()){
+            System.out.println("Underflow");
+            return;
+        }
+        System.out.println(arr[0]);
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public int size(){
+        return size;
+    }
+    public void display(){
+        System.out.println(Arrays.toString(arr));
+        displayPretty(0, "", true);
+    }
+    private void displayPretty(int i, String string, boolean b) {
+        // TODO Auto-generated method stub
+        if(i>=size || arr[i]==-1) return;
+        System.out.print(string);
+        System.out.print(b ? "├── Left -  " : "└── Right - ");
+        System.out.println(arr[i]);
+        displayPretty((2*i)+1, string + (b ? "│   " : "    "), true);
+        displayPretty((2*i)+2, string + (b ? "│   " : "    "), false);
+    }
+    public static void main(String[] args) {
+        MinHeap minHeap = new MinHeap();
+        minHeap.add(2);
+        minHeap.add(1);
+        minHeap.add(3);
+        minHeap.add(4);
+        minHeap.add(0);
+        minHeap.add(2);
+        minHeap.display();
+        minHeap.peek();
+        minHeap.pop();
+        minHeap.pop();
+        minHeap.pop();
+        minHeap.display();
+    }
+}
